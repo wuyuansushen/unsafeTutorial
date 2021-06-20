587ff5f185b67273222293dcf8917baeb1bf053f
wuyuansushen <wuyuansushen@gmail.com>
2021-06-20 19:48:23 +0800

    Pointer

--- unsafeTutorial/Program.cs
+++ unsafeTutorial/Program.cs
@@ -16,7 +16,9 @@ namespace unsafeTutorial
             int* p2 = &i;
             doubleInt(p1);
 
-            Console.WriteLine($"{i}\n{*p1}\n{*p2}");
+            Console.WriteLine($"Value: {i,23}\nPointer1: {*p1,20}\nPointer2: {*p2,20}");
+            Console.WriteLine($"ValueAddress: {(int)&i,16:X}\nPointer1Value: {(int)p1,15:X}\nPointer1Address: {(int)&p1,13:X}\n" +
+                $"Pointer2Value: {(int)p2,15:X}\nPointer2Address: {(int)&p2,13:X}");
         }
     }
 }
